92e4a68d1a6f3bb5965e989ea05931ff931092f6
Abhishek Bagela <abhishek.bagela@quantic.co.in>
2022-03-30 01:00:22 +0530

    Initial Commit

---
+++ CoreDataFundamentals/CoreDataFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataFundamentalsApp.swift
+//  CoreDataFundamentals
+//
+//  Created by Abhishek Bagela on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
